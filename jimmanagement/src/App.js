4d3b30010cbdedc56bc157b767438e7eb71f8d09
ruthasoll <ruths9317@gmail.com>
2023-03-22 13:57:27 +0300

    conflict resolved and Login page added

--- jimmanagement/src/App.js
+++ jimmanagement/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/navbar';
+import Navibar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-     <Login/>
+     <Navibar/>
     </div>
   );
 }
