9ad5f23c83436e6c950926e1da3bf901ee15b540
ruthasoll <ruths9317@gmail.com>
2023-03-22 12:57:22 +0300

    navigation problems fixed

--- jimmanagement/src/components/navbar.js
+++ jimmanagement/src/components/navbar.js
@@ -1,41 +1,24 @@
 const Login = () => {
     return(
         <div>
-            <div class="mt-4 ml-6">Fitness Jim</div>
-           <nav className="navbar">
-            <div className="Links">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            
+           <nav className="navbabbbr">
+         
+            <nav class="navbar bg-body-tertiary bg-secondary">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+    <a class="navbar-brand">Navbar</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
-                {/* <a href="/Join">Join</a>
-                <a href="/about">About us</a>
-                <a href="message">Message us</a> */}
+</nav>
 
+              <div class="mt-4 ml-6">Fitness Jim</div>
             </div>
-        </nav> 
-        </div>
+     
+   
         
     );
 } 
